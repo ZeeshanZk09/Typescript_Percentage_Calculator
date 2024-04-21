03aca539c39715b67c1607704b6809a5ddd6499d
Entreprenuer Zeeshan <dr5269139@gmail.com>
2024-04-21 17:02:55 +0500

    Typescript_Percentage_Calculator

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// This code calculate percentages for 5 subjects 
+// Subjects: Maths, Physics, Chemistry, English, Biology
+// Student Detail
+let StudentId = "877DNSKJ";
+// Total Marks
+let MaxMarks = 500;
+// Students Marks
+let MathScore = 85;
+let PhysicsScore = 97;
+let ChemistryScore = 35;
+let EnglishScore = 55;
+let BiologyScore = 77;
+let FinalResult = (MathScore + PhysicsScore + ChemistryScore + EnglishScore + BiologyScore);
+let ResultStatement = "Student with Id " + '' + StudentId + '' + " obtained percentage = " + (FinalResult / MaxMarks) * 100 + "%";
+console.log(ResultStatement);
